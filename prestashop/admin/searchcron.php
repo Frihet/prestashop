4be88034f39d7e0e9c7a786906d756a47fdc9e32
root <root@shp.freecode.no>
2009-09-17 07:43:10 +0000

    Oups, forgot to remove install

--- prestashop/admin/searchcron.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include(dirname(__FILE__).'/../config/config.inc.php');
-
-ini_set('max_execution_time', 600);
-Search::indexation(Tools::getValue('full'));
-Tools::redirectAdmin($_SERVER['HTTP_REFERER'].'&conf=4');
-
-?>
