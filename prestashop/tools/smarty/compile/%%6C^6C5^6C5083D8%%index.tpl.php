9db436fae59e936193c27c6ad97f776f4e2ad440
root <root@shp.freecode.no>
2009-10-02 08:26:10 +0000

    Removed smarty cache from git

--- prestashop/tools/smarty/compile/%%6C^6C5^6C5083D8%%index.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2009-08-24 11:33:13
-         compiled from /srv/www/shp-dev/prestashop/themes/prestashop/index.tpl */ ?>
-<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
