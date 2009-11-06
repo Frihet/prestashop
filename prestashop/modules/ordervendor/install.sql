9ea2aa7315450b52bdd0ebb9641e604b39ec9719
root <root@shp.freecode.no>
2009-11-06 12:50:45 +0000

    Added id_customer to vendors

--- prestashop/modules/ordervendor/install.sql
+++ prestashop/modules/ordervendor/install.sql
@@ -10,12 +10,14 @@ CREATE TABLE IF NOT EXISTS `PREFIX_postcode` (
 
 CREATE TABLE IF NOT EXISTS `PREFIX_vendor` (
   `id_vendor` int(10) unsigned NOT NULL auto_increment,
+  `id_customer` int(10) unsigned,
   `id_postcode` int(10) NOT NULL,
   `title` varchar(256) NOT NULL,
   `distance` float(10) unsigned NOT NULL,
   `date_add` datetime NOT NULL,
   `date_upd` datetime NOT NULL,
   FOREIGN KEY (`id_postcode`) REFERENCES `PREFIX_postcode`(`id_postcode`),
+  FOREIGN KEY (`id_customer`) REFERENCES `PREFIX_customer`(`id_customer`),
   PRIMARY KEY (`id_vendor`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
