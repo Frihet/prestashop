c7dff6bd4c62a43cbf52812e30fc3598b057250e
root <root@shp-prod.freecode.no>
2010-10-13 16:46:46 +0200

    Added sortcategories

---
+++ prestashop/modules/sortcategories/en.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{sortcategories}prestashop>sortcategories_eb5bb625788a8a8b08f8f8c843b719ad'] = 'Sort Categories ';
+$_MODULE['<{sortcategories}prestashop>sortcategories_e9b916dbd368a8baef60027ebefdf6d6'] = 'Allows you to sort the categories manually. ';
+$_MODULE['<{sortcategories}prestashop>sortcategories_21f8c4dec0b8f29305a859e65393ceb5'] = 'remove this module?';
+$_MODULE['<{sortcategories}prestashop>sortcategories_d08c14db6f33c83a38d117ab56bfbc58'] = 'Sort Manually';
+$_MODULE['<{sortcategories}prestashop>sortcategories_1ad19c20cc4402c32e5f9d5ac438dc2b'] = 'Back to Home';
+$_MODULE['<{sortcategories}prestashop>sortcategories_76b56638af22c17c4b07b74ada232fdb'] = 'Save';
---
+++ prestashop/modules/sortcategories/no.php
@@ -0,0 +1,5 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{sortcategories}prestashop>sortcategories_1ca14d681b9139eed471205d5f352e10'] = 'Sorter Kategorier';
